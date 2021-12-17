0b91270c65a5e8360f6fb96569b8dbd42faca948
asakhar <a@sakhar.ru>
2021-12-17 23:22:24 +0300

    reformat

--- check_random.py
+++ check_random.py
@@ -7,11 +7,12 @@ seedmax = 460
 lenmin = 5000
 lenmax = 5002
 seq = 1
-for seed in range(seedmin,seedmax):
-  for length in range(lenmin,lenmax):
+for seed in range(seedmin, seedmax):
+  for length in range(lenmin, lenmax):
     res = None
     for _ in range(seq):
-      f = subprocess.run(["build/main", f"{length}", f"{seed}"], capture_output=True)
+      f = subprocess.run(
+          ["build/main", f"{length}", f"{seed}"], capture_output=True)
       line1, line2 = f.stdout.decode('UTF-8').split('\n')[:2]
       assert line1 == line2[::-1], "Lines are not inverse of each other"
       if res is None:
@@ -26,12 +27,13 @@ for seed in range(seedmin,seedmax):
               j = pj
               i = pi
               break
-            i+=1
-            j+=1
-          if (i == len(line1) or j == len(line1)) and i-pi>length//4:
+            i += 1
+            j += 1
+          if (i == len(line1) or j == len(line1)) and i-pi > length//4:
             print(f"random seq len cycle length = {i-pi}")
           i += 1
         continue
       assert res == line1, "Lines with same length and same seed should be equal"
 
-print(f"Checked {seedmax-seedmin} seeds, {lenmax-lenmin} lengths, each {seq} times")
+print(
+    f"Checked {seedmax-seedmin} seeds, {lenmax-lenmin} lengths, each {seq} times")
